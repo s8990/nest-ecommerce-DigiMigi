3958d80e4b98aee7bbdb6c592243d67017c5de90
saeed <s8990@yahoo.com>
2022-12-23 01:13:22 +0330

    add some class-validator to DTOs

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+  Matches,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
@@ -15,16 +22,18 @@ export class CreateUserDto {
 
   @IsNotEmpty()
   @IsString()
+  @Matches(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
   readonly mobile: string;
 
   @IsNotEmpty()
   @IsString()
+  @MinLength(6)
   readonly password: string;
 
   @IsOptional()
   @IsString()
   readonly bio: string;
-  
+
   @IsOptional()
   @IsString()
   readonly image: string;
